fbd1579bd9fb3da388cb70f45cb3420878f93cf8
Charles Strahan <charles.c.strahan@gmail.com>
2013-09-02 22:09:56 -0400

    update readme, make CXX and CXXFLAGS configurable

--- README.md
+++ README.md
@@ -6,6 +6,22 @@
 
 This here is a [Ruby][ruby] wrapper for the official C++ implementation of [Cap'n Proto][capnp].
 
+# Installing
+
+First [install libcapnp][libcapnp-install], then install the gem:
+
+```bash
+gem install capn_proto
+```
+
+Remember to set the CXX and CXXFLAGS environment variables as necessary. As an OSX user, having followed the [instructions for installing libcapnp on OSX][libcapnp-install], the correct incantation is as follows:
+
+```bash
+export CXX=$HOME/clang-3.2/bin/clang++
+export CXXFLAGS="-std=c++11 -stdlib=libc++"
+gem install capn_proto
+```
+
 # Status
 
 - [x] Schema parsing (and recursive Module definition)
@@ -18,3 +34,4 @@ Proper support for [JRuby][jruby] will come after I implement support for Java.
 [ruby]: http://www.ruby-lang.org/ "Ruby"
 [capnp]: http://kentonv.github.io/capnproto/ "Cap'n Proto"
 [jruby]: http://jruby.org/ "JRuby"
+[libcapnp-install]: [http://kentonv.github.io/capnproto/install.html]
--- ext/ruby_capn_proto/extconf.rb
+++ ext/ruby_capn_proto/extconf.rb
@@ -1,6 +1,6 @@
 require 'mkmf'
 
-CONFIG['CXX'] = "/Users/charlesstrahan/clang-3.2/bin/clang++"
+CONFIG['CXX'] = ENV['CXX'] if ENV['CXX']
 CONFIG['CXXFLAGS'] += " -std=c++11 -stdlib=libc++"
 
 if enable_config('debug')
@@ -11,21 +11,17 @@ $LDFLAGS += " -lcapnpc"
 $LDFLAGS += " -lcapnp"
 $LDFLAGS += " -lkj"
 
-HEADER_DIRS = ["/usr/local/include"]
-LIB_DIRS    = ["/usr/local/lib"]
-
 create_makefile('ruby_capn_proto/init')
 
-
 # HACK ATTACK
-def inreplace(path, search, replace)
-  contents = File.read(path)
-  contents.gsub!(search, replace)
-  File.open(path, "w") do |f|
-    f.write(contents)
-  end
-end
+# def inreplace(path, search, replace)
+  # contents = File.read(path)
+  # contents.gsub!(search, replace)
+  # File.open(path, "w") do |f|
+    # f.write(contents)
+  # end
+# end
 
-inreplace("Makefile", "V = 0", "V = 1")
-inreplace("Makefile", "-O3", "-O0")
-inreplace("Makefile", "ECHO = $(ECHO1:0=@echo)", "ECHO = @echo")
+# inreplace("Makefile", "V = 0", "V = 1")
+# inreplace("Makefile", "-O3", "-O0")
+# inreplace("Makefile", "ECHO = $(ECHO1:0=@echo)", "ECHO = @echo")
